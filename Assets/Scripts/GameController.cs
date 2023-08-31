8fd1ab0befa73f6807df1ff8310acb7da5eb611f
Khadeja22 <112954675+Khadeja22@users.noreply.github.com>
2023-08-31 18:36:29 -0400

    finished key component (need to work on transitioning to new scene after truekey and relating health to key attempts)

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -1,25 +1,45 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using System.Collections;
+
 
 public class GameController : MonoBehaviour
 {
     public float timeLimit = 20f;
     private float currentTime;
+    public GameObject messagePanel; // Reference to the UI Panel
+    public Text timerText; // Reference to the UI Text
+    private bool timerExpired = false;
 
     void Start()
     {
         currentTime = timeLimit;
+        messagePanel.SetActive(false); // Disable the panel initially
     }
 
     void Update()
     {
         currentTime -= Time.deltaTime;
 
-        if (currentTime <= 0f)
+        if (currentTime <= 0f && !timerExpired)
         {
-            // Restart the current scene
-            Scene scene = SceneManager.GetActiveScene();
-            SceneManager.LoadScene(scene.name);
+            // Display the message panel and timer text
+            messagePanel.SetActive(true);
+            timerText.text = "YOU DIDNT FIND THE KEY IN TIME! Try Again";
+            timerExpired = true;
+
+            // Start the coroutine to hide the message after 5 seconds
+            StartCoroutine(HideMessageAfterDelay(5f));
         }
+    }
+
+    IEnumerator HideMessageAfterDelay(float delay)
+    {
+        yield return new WaitForSeconds(delay);
+
+        // Hide the message panel and reset the timer text
+        messagePanel.SetActive(false);
+        timerText.text = "";
     }
 }
